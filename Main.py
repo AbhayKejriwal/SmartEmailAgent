1ebe40313a434ad023b09bacffd5a4fd2bf81b1e
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-10-07 16:08:44 +0530

    Refactor createInitialLabels function and add error handling for response generation and processing

--- Main.py
+++ Main.py
@@ -4,27 +4,34 @@ import json
 import os
 
 def createInitialLabels():
-  ml.create_label("Important Emails", "#000000", "#cc3a21")
+  ml.create_label("Priority", "#000000", "#cc3a21")
   ml.create_label("Not Important", "#000000", "#ffad47")
   ml.create_label("Spam", "#000000", "#4a86e8")
-  ml.create_label("Cannot Classify", "#000000", "#cccccc")
+  # ml.create_label("Cannot Classify", "#000000", "#cccccc")
 
 def processEmails(emails):
   for email in emails:
       # message = email.pop('Message')
       try:
         response = astn.generate(email)
+      except:
+        print("Error in generating response.")
         # print(response)
         res = json.loads(response) # Parse the JSON string into a dictionary
         email['Summary'] = res['Summary']
         email['Category'] = res['Category'] 
-        ml.addLabels(email, [res['Category']])
+      try:
+        if res['Category'] != 'Cannot Classify':
+          ml.addLabels(email, [res['Category']])
+        if res['Category'] == 'Not Important' or res['Category'] == 'Spam':
+          ml.removeLabels(email, ['INBOX'])
       except:
         print("Error in processing response.")
 
 def main():
   labels = ['INBOX'] # this line specifies the label of the emails to be read
   state = "is:unread" # this line specifies the state of the emails to be read
+  createInitialLabels()
   emails = ml.getEmails(labels, state)
   
   if not emails:
