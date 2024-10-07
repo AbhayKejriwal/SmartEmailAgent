b164b747967e732474f937c8942c1915d4a02a91
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-10-07 15:43:21 +0530

    Update EmailAsst.py to load user preferences from Prefs.txt

--- EmailAsst.py
+++ EmailAsst.py
@@ -4,22 +4,19 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-with open("Prefs.json", "r", encoding="utf-8") as f:
-  prefs = json.load(f)
-  user_prefs = prefs.get("user_prefs", {})
-  system_prefs = prefs.get("system_prefs", {})
+with open("Prefs.txt", "r", encoding="utf-8") as f:
+  prefs = f.read()
 
 
 def generate(message):
   genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
   model = genai.GenerativeModel(model_name="gemini-1.5-flash")
   
-  prompt = """
-    You are a personal email assistant. Your job is to know the user's priorities, preferences and requirements about their email and filter out the content and display it in the set format.
+  prompt = """You are a personal email assistant. Your job is to know the user's priorities, preferences and requirements about their email and filter out the content and display it in the set format.
 
-    #User Preferences & Special Instructions: """ + user_prefs + """
+    #User Preferences & Special Instructions: """ + prefs + """
 
-    The email message is provide in HTML format and the output should be strictly returned in the specified JSON format with no other prefix or suffix. Filter the right content according to my requirements, provide a brief summary of the mail content and categorize the mail. The various categories are: \"Important\", \"NotImportant\", \"Spam\" and \"CannotClasify\"(not enough data in preferences to categorize the mail).
+    The email message is provide in HTML format and the output should be strictly returned in the specified JSON format with no other prefix or suffix. Filter the right content according to my requirements, provide a brief summary of the mail content and categorize the mail. The various default categories are: \"Priority\", \"NotImportant\", \"Spam\" and \"CannotClasify\"(not enough data in preferences to categorize the mail).
 
     OUTPUT FORMAT:
     {
@@ -32,9 +29,13 @@ def generate(message):
     
     """
   # print(prompt)
-  response = model.generate_content(prompt)
-  
-  return response.text
+  try:
+    response = model.generate_content(prompt)
+    return response.text
+  except Exception as e:
+    print("Error in generating response.")
+    print(e)
+    return None
 
 def main():
   with open("1917a9d2c104454d.html", "r", encoding="utf-8") as f:
