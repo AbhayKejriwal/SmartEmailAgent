dd28c47d164f615c262b538a5823bede7971e34b
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-10-07 15:43:36 +0530

    Refactor Main.py to create initial labels and process emails

--- Main.py
+++ Main.py
@@ -1,8 +1,26 @@
 import GmailAPI as ml
-import FileManagement as fl
 import EmailAsst as astn
 import json
 import os
+
+def createInitialLabels():
+  ml.create_label("Important Emails", "#000000", "#cc3a21")
+  ml.create_label("Not Important", "#000000", "#ffad47")
+  ml.create_label("Spam", "#000000", "#4a86e8")
+  ml.create_label("Cannot Classify", "#000000", "#cccccc")
+
+def processEmails(emails):
+  for email in emails:
+      # message = email.pop('Message')
+      try:
+        response = astn.generate(email)
+        # print(response)
+        res = json.loads(response) # Parse the JSON string into a dictionary
+        email['Summary'] = res['Summary']
+        email['Category'] = res['Category'] 
+        ml.addLabels(email, [res['Category']])
+      except:
+        print("Error in processing response.")
 
 def main():
   labels = ['INBOX'] # this line specifies the label of the emails to be read
@@ -12,36 +30,7 @@ def main():
   if not emails:
     print("No emails found.")
   else:
-    for email in emails:
-      message = email.pop('Message')
-    # Save the email message as an html file
-      with open(str(email['MessageID'])+'.html', 'w', encoding='utf-8') as file:
-        file.write(message)
-        print("Email saved as " + str(email['MessageID']) + ".html")
-        #save the file link in the email data
-        email['MessageFile'] = os.getcwd() + "\\" +str(email['MessageID']) + ".html"
-      # get output for each email,  and save it in a new df to later safe to excel
-      response = astn.generate(message)
-      print(response)
-      # save the response in a new file with same message id summary as name and .txt extension
-      filename = str(email['MessageID']) + "_summary.txt"
-      with open(filename, 'w', encoding='utf-8') as file:
-        file.write(response)
-        print("Summary saved as " + filename)
-        email['SummaryFile'] = os.getcwd() + "\\" + filename 
-      
-      # Parse the JSON string into a dictionary
-      print(response)
-      try:
-        res = json.loads(response)
-        # email['Filtered_Message'] = res['Filtered_Message']
-        email['Summary'] = res['Summary']
-        email['Category'] = res['Category'] 
-      except:
-        print("Error in processing response.") 
-    # save the rest of the emails data in a text file
-    fl.save_to_excel(emails, "emails.xlsx")
-    print("Email data saved in emails.xlsx.")
+    processEmails(emails)
 
 if __name__ == "__main__":
   main()
