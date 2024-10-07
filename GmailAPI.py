1e2a78b51b3a17ef91874d92376ace421b1a9800
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-10-07 15:43:29 +0530

    Refactor getEmail function to remove unnecessary comments and improve code readability

--- GmailAPI.py
+++ GmailAPI.py
@@ -38,21 +38,6 @@ def getCredentials():
   return creds
 
 def getEmail(service, message):
-  """
-  The function `getEmail` retrieves and processes email information from a specified message using the
-  Gmail API in Python.
-  
-  :param service: The `service` parameter in the `getEmail` function is typically an instance of a
-  Gmail service object that allows interaction with the Gmail API. This object is used to retrieve
-  information about a specific email message identified by its ID
-  
-  :param message: The function `getEmail` takes two parameters: `service` and `message`. The `service`
-  parameter is expected to be a Gmail service object, and the `message` parameter is expected to be a
-  dictionary representing a message retrieved from Gmail
-  
-  :return: The `getEmail` function is returning a dictionary containing the MessageID, Subject, From,
-  and Message content of the email.
-  """
   msg = service.users().messages().get(userId='me', id=message['id']).execute()
   
   payload = msg.get('payload')
@@ -66,6 +51,7 @@ def getEmail(service, message):
   text = ""
 
   parts = payload.get('parts')
+  
   for part in parts:
     data = part['body'].get('data', '')  # Safely retrieve the value of 'data' key with a default value of an empty string
     byte_code = base64.urlsafe_b64decode(data)
@@ -73,26 +59,8 @@ def getEmail(service, message):
   email = { 'MessageID': message['id'], 'Subject': subject, 'From': sender, 'Message': text }
 
   return email
-  # msg  = service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['UNREAD']}).execute()
 
 def getEmails(labels=[], state=None):
-  """
-  The function `getEmails` uses the Gmail API to retrieve emails based on specified labels and state
-  parameters.
-  
-  :param labels: The `labels` parameter in the `getEmails` function is used to specify the labels of
-  the emails you want to retrieve. Labels are like folders in Gmail that help you organize your
-  emails. By providing label IDs in the `labels` parameter, you can filter the emails based on those
-  labels
-  
-  :param state: The `state` parameter in the `getEmails` function is used to filter the emails based
-  on a specific query string. If provided, it will search for emails that match the specified query
-  string in the Gmail account
-  
-  :return: The `getEmails` function returns a list of email messages based on the specified labels and
-  state parameters. It interacts with the Gmail API to retrieve messages from the user's Gmail
-  account.
-  """
   # Call the Gmail API
   creds = getCredentials() # this line gets the credentials of the user to access the gmail account
   service = build('gmail', 'v1', credentials=creds) # this line creates a service object that interacts with the Gmail API
@@ -113,6 +81,12 @@ def getEmails(labels=[], state=None):
     emails.append(email)
 
   return emails
+
+def deleteEmail(email):
+  creds = getCredentials()
+  service = build('gmail', 'v1', credentials=creds)
+  msg = service.users().messages().delete(userId='me', id=email['MessageID']).execute()
+  return msg
 
 def getLabels():
   creds = getCredentials() # this line gets the credentials of the user to access the gmail account
@@ -160,18 +134,26 @@ def addLabels(email, labels):
     service = build('gmail', 'v1', credentials=creds)
     label_ids = []
     for label in labels:
-      label_ids.append(get_label_id(label))
+      label_id = get_label_id(label)
+      if not label_id:
+        label = create_label(label)
+        label_id = label['id']
+      label_ids.append(label_id)
     service.users().messages().modify(userId='me', id=email['MessageID'], body={'addLabelIds': label_ids}).execute()
     print(f"Label {labels} added to email {email['Subject']}.")
   except Exception as e:
     print(f"An error occurred while adding label to email: {e}")
 
-def create_label(label_name):
+def create_label(label_name, text_color="#000000", background_color="#ffffff"):
   """Creates a new label in the user's Gmail account."""
   label_body = {
   "labelListVisibility": "labelShow",
   "messageListVisibility": "show",
   "name": label_name,
+  "color": {
+          "textColor": text_color,
+          "backgroundColor": background_color
+      }
   }
   creds = getCredentials()
   service = build('gmail', 'v1', credentials=creds)
@@ -204,73 +186,18 @@ def modify_label_color(label_id, text_color, background_color):
 
 # Driver and Test code
 def main():
-  labels = ['INBOX']
-  state = "is:unread"
-  # modify_label_color('Label_5', '#ffffff', '#000000') - invalid colour codes
+  # labels = ['INBOX']
+  # state = "is:read"
   # emails = getEmails(labels, state)
-
   # if not emails:
   #   print("No emails found.")
   # else:
   #   for email in emails:
   #     print(email['Subject'], email['From'])#, email['Message'])
   #     removeLabels(email, ['Not Important'])
-      # # Store the email in a text file
-      # with open('email.txt', 'w') as file:
-      #   file.write(str(email))
-  # create_label('Not Important')
-  # print(getLabels())
-  # print(get_label_id('Not Important'))
+  #     addLabels(email, ['Priority'])
+  # create_label("Priority", "#000000", "#cc3a21")
 
-
-
-def fetchAllEmails():
-  emails = getEmails()
-  return emails
-
-def fetchInbox():
-  labels = ['INBOX']
-  inbox = getEmails(labels)
-  return inbox
-
-def fetchStarred():
-  labels = ['STARRED']
-  starred = getEmails(labels)
-  return starred
-
-def fetchUnread():
-  state = "is:unread"
-  unread = getEmails(state)
-  return unread
-
-def fetchRead():
-  state = "is:read"
-  read = getEmails(state)
-  return read
-
-def markRead(email):
-  removeLabels(email, ['UNREAD'])
-
-def markUnread(email):
-  addLabels(email, ['UNREAD'])
-
-def markStarred(email):
-  addLabels(email, ['STARRED'])
-
-def markUnstarred(email):
-  removeLabels(email, ['STARRED'])
-
-def deleteEmail(email):
-  creds = getCredentials()
-  service = build('gmail', 'v1', credentials=creds)
-  msg = service.users().messages().delete(userId='me', id=email['MessageID']).execute()
-  return msg
-
-def archiveEmail(email):
-  creds = getCredentials()
-  service = build('gmail', 'v1', credentials=creds)
-  msg = service.users().messages().modify(userId='me', id=email['MessageID'], body={'removeLabelIds': ['INBOX']}).execute()
-  return msg
 
 # Run the main function
 if __name__ == '__main__':
