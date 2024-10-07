a5bb9f98364b5f7c16f9ee00960d54526b7f72fe
Abhay Kejriwal <121892598+AbhayKejriwal@users.noreply.github.com>
2024-10-07 18:54:28 +0530

    Refactor processEmails function to handle emails in batches

--- Main.py
+++ Main.py
@@ -1,43 +1,53 @@
 import GmailAPI as ml
 import EmailAsst as astn
 import json
-import os
+
 
 def createInitialLabels():
   ml.create_label("Priority", "#000000", "#cc3a21")
   ml.create_label("Not Important", "#000000", "#ffad47")
-  ml.create_label("Spam", "#000000", "#4a86e8")
+  ml.create_label("Useless", "#000000", "#4a86e8")
   # ml.create_label("Cannot Classify", "#000000", "#cccccc")
 
-def processEmails(emails):
+
+def processEmails(emails, count):
   for email in emails:
-      # message = email.pop('Message')
-      try:
-        response = astn.generate(email)
-      except:
-        print("Error in generating response.")
-        # print(response)
-        res = json.loads(response) # Parse the JSON string into a dictionary
-        email['Summary'] = res['Summary']
-        email['Category'] = res['Category'] 
-      try:
-        if res['Category'] != 'Cannot Classify':
-          ml.addLabels(email, [res['Category']])
-        if res['Category'] == 'Not Important' or res['Category'] == 'Spam':
-          ml.removeLabels(email, ['INBOX'])
-      except:
-        print("Error in processing response.")
+    response = astn.generate(str(email))
+    res = json.loads(response) # Parse the JSON string into a dictionary
+    print(res)
+    
+    email['Summary'] = res['Summary']
+    email['Category'] = res['Category'] 
+
+    if res['Category'] != 'Cannot Classify':
+      ml.addLabels(email, [res['Category']])
+      if res['Category'] != 'Priority':
+        ml.removeLabels(email, ['INBOX'])
+    print("Emails Processed:", count)
+    count += 1
+  return count
+
 
 def main():
   labels = ['INBOX'] # this line specifies the label of the emails to be read
   state = "is:unread" # this line specifies the state of the emails to be read
-  createInitialLabels()
-  emails = ml.getEmails(labels, state)
   
-  if not emails:
+  # emails = ml.getEmails(labels, state)
+  mails = ml.listEmails(labels, state)
+  batch_size = 10
+  
+  if not mails:
     print("No emails found.")
   else:
-    processEmails(emails)
+    print(len(mails), "emails found.")
+    count = 1
+    for i in range(0, len(mails), batch_size):
+      batch = mails[i:i+batch_size]
+      print(batch)
+      emails = ml.getEmails(batch)
+      count = processEmails(emails, count)
+    else:
+      print("No more emails.")
 
 if __name__ == "__main__":
   main()
